38f93580b35702771104b1cef0f791573a71eb21
paulshervin92-sys <paulshervin92@gmail.com>
2026-02-07 22:50:31 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    allowedHosts: "all"
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  preview: {
+    allowedHosts: "all"
+  }
+})
